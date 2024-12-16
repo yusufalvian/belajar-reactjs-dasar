eee4c523bdc3d2055887f06f135649d74321c434
khannedy <echo.khannedy@gmail.com>
2024-12-16 19:56:14 +0700

    Update counter logic to increment by 3 and improve logging
    Updated the counter increment function to add 3 instead of 1. Additionally, refined the console log to display the current counter value during rendering for better debugging.

--- src/form/Counter.jsx
+++ src/form/Counter.jsx
@@ -3,10 +3,11 @@ import {useState} from "react";
 export default function Counter(){
     let [counter, setCounter] = useState(0);
 
-    console.info(`Render Counter`);
+    console.info(`Render Counter ${counter}`);
 
     function handleClick(){
-        setCounter(counter + 1);
+        setCounter(counter + 3);
+        // masih 0
         console.log(counter);
     }
 
