60144e31fc17b60e269c9d0075605fbbdbf56246
khannedy <echo.khannedy@gmail.com>
2024-12-16 15:35:44 +0700

    Refactor TodoList to use mapped data for todos
    Replaced hardcoded Todo components with a dynamic map function over a predefined data array. This improves the code's maintainability and scalability by centralizing the todo data and enabling easier modifications or extensions in the future.

--- src/todolist/TodoList.jsx
+++ src/todolist/TodoList.jsx
@@ -1,12 +1,39 @@
 import Todo from "./Todo.jsx";
 
 export default function TodoList() {
+    const data = [
+        {
+            id: 0,
+            text: "Learn HTML",
+            isCompleted: true
+        },
+        {
+            id: 1,
+            text: "Learn CSS",
+            isCompleted: true
+        },
+        {
+            id: 2,
+            text: "Learn JavaScript",
+            isCompleted: true
+        },
+        {
+            id: 3,
+            text: "Learn ReactJS",
+            isCompleted: false
+        },
+        {
+            id: 4,
+            text: "Learn ReactJS Router",
+            isCompleted: false
+        }
+    ]
+
     return (
         <ul>
-            <Todo isCompleted={true} text="Learn HTML" isDeleted={true}/>
-            <Todo isCompleted={true} text="Learn CSS"/>
-            <Todo isCompleted={true} text="Learn JavaScript"/>
-            <Todo isCompleted={false} text="Learn ReactJS"/>
+            {data.map((todo) => (
+                <Todo key={todo.id} {...todo}/>
+            ))}
         </ul>
     )
 }
