f030362a65792506ecba694491152c774cabfb68
khannedy <echo.khannedy@gmail.com>
2024-12-16 14:52:14 +0700

    Add Container component to wrap HelloWorld output
    Introduced a new `Container` component to provide a consistent structure including a header and footer. Updated `main.jsx` to render `HelloWorld` within the `Container`.

---
+++ src/hello-world/Container.jsx
@@ -0,0 +1,11 @@
+export default function Container({children}){
+    return (
+        <div>
+            <h1>Programmer Zaman Now</h1>
+            {children}
+            <footer>
+                <p>2024 Programmer Zaman Now</p>
+            </footer>
+        </div>
+    )
+}
--- src/hello-world/main.jsx
+++ src/hello-world/main.jsx
@@ -1,10 +1,13 @@
 import {createRoot} from "react-dom/client";
 import HelloWorld from "./HelloWorld.jsx";
 import {StrictMode} from "react";
+import Container from "./Container.jsx";
 
 createRoot(document.getElementById("root"))
     .render(
         <StrictMode>
-            <HelloWorld/>
+            <Container>
+                <HelloWorld/>
+            </Container>
         </StrictMode>
     )
