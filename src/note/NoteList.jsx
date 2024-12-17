3bada5cb4e28b1216c2b5dc6f08ed5cd5ebf8b8d
khannedy <echo.khannedy@gmail.com>
2024-12-17 12:26:02 +0700

    Add search functionality to NoteList component
    Introduced a search bar to filter notes by text, utilizing `useState`, `useRef`, and `useMemo` for efficient state handling and rendering. This update enhances usability by allowing users to quickly find specific notes.

--- src/note/NoteList.jsx
+++ src/note/NoteList.jsx
@@ -1,16 +1,33 @@
 import Note from "./Note.jsx";
-import {useContext} from "react";
+import {useContext, useMemo, useRef, useState} from "react";
 import {NotesContext} from "./NoteContext.jsx";
 
 export default function NoteList() {
     const notes = useContext(NotesContext);
+    const [search, setSearch] = useState("");
+    const searchInput = useRef(null);
+
+    const filteredNotes = useMemo(() => {
+        console.info('Filtering notes')
+        return notes.filter(note => note.text.includes(search))
+    }, [notes, search])
+
+    function handleSearch(){
+        console.info('Search')
+        setSearch(searchInput.current.value);
+    }
+
     return (
-        <ul>
-            {notes.map(note => (
-                <li key={note.id}>
-                    <Note note={note}/>
-                </li>
-            ))}
-        </ul>
+        <div>
+            <input ref={searchInput} placeholder="Search"/>
+            <button onClick={handleSearch}>Search</button>
+            <ul>
+                {filteredNotes.map(note => (
+                    <li key={note.id}>
+                        <Note note={note}/>
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
