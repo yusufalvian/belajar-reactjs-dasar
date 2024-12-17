3c3cb340e91a1615a68502fb69e98f773d855b6b
khannedy <echo.khannedy@gmail.com>
2024-12-17 10:14:28 +0700

    Add button to manually trigger product loading
    Replaced useRef with useState for tracking loading state. Added a button to allow users to manually initiate the product fetch process. Adjusted the useEffect dependency to respond to the updated load state.

--- src/product/ProductList.jsx
+++ src/product/ProductList.jsx
@@ -1,27 +1,36 @@
-import {useEffect, useRef, useState} from "react";
+import {useEffect, useState} from "react";
 import Product from "./Product.jsx";
 
 export default function ProductList() {
     const [products, setProducts] = useState([]);
-    const loaded = useRef(false)
+    const [load, setLoad] = useState(false);
+
+    function handleClick(){
+        console.info(`Handle Click`);
+        setLoad(true);
+    }
+
+    useEffect(() => {
+        console.info(`Call Use Effect with []`)
+    }, []);
 
     useEffect(() => {
         console.info('Call Use Effect');
-        if(loaded.current === false){
+        if(load){
             fetch("/products.json")
                 .then((response) => response.json())
-                .then((data) => setProducts(data))
-                .then(() => loaded.current = true);
+                .then((data) => setProducts(data));
         }
 
         return () => {
             console.info("Product List component unmounted");
         }
-    });
+    }, [load]);
 
     return (
         <>
             <h1>Product List</h1>
+            <button onClick={handleClick}>Load Products</button>
             {products.map((product) => (
                 <Product product={product} key={product.id}/>
             ))}
